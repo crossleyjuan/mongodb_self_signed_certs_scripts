b2094114f0cbccfd89a2576d96adefdca50715c7
Juan Pablo Crossley <Cross>
2023-02-07 11:43:04 +0100

    Minor change in the ca.sh to generate the ca.pem instead of ca-pem

--- ca.sh
+++ ca.sh
@@ -4,5 +4,5 @@ source init
 # Creates the private key
 openssl genrsa -out certs/ca.key 4096
 # creates the CA certificate
-openssl req -x509 -new -nodes -key certs/ca.key -sha256 -days 1024 -config openssl-test-ca.cnf -out certs/ca-pem -extensions v3_ca
+openssl req -x509 -new -nodes -key certs/ca.key -sha256 -days 1024 -config openssl-test-ca.cnf -out certs/ca.pem -extensions v3_ca
 
